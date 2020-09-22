5d15c0a5abf6fd00e11288f7ca46c1bc8fa9c8a5
angfrase <angela.n.fraser@gmail.com>
2020-09-21 23:22:38 -0400

    audio in progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,32 @@
-let team1shoot = document.querySelector("#teamone-shoot-button");
-let team1numbershoot = document.querySelector("#teamone-numshots");
+let team1 = document.querySelector("#teamone-shoot-button");
+let team1shots = document.querySelector("#teamone-numshots");
 let team1goals=document.querySelector("#teamone-numgoals");
 
-team1shoot.addEventListener("click", function () {
+team1.addEventListener("click", function () {
 
     console.log("team one shoot");
-    let team1number = Number(team1numbershoot.innerHTML) + 1;
-    team1numbershoot.innerHTML=team1number;
+    let team1number = Number(team1shots.innerHTML) + 1;
+    team1shots.innerHTML=team1number;
         if (Math.random() * 100 < 65 ) {
             let scoregoals=Number(team1goals.innerHTML) + 1;
         team1goals.innerHTML = scoregoals;
     }
 })
+// let x = document.getElementById("My Audio");
+// function playAudio(){
+//     x.play();
+// }
+// function pauseAudio() {
+//     x.pause();
+// }
 
-let team2shoot = document.querySelector("#teamtwo-shoot-button");
-let team2numbershoot = document.querySelector("#teamtwo-numshots");
+let team2= document.querySelector("#teamtwo-shoot-button");
+let team2shots = document.querySelector("#teamtwo-numshots");
 let team2goals=document.querySelector("#teamtwo-numgoals")
-team2shoot.addEventListener("click", function () {
+team2.addEventListener("click", function () {
     console.log("team two shoot");
-    let team2number = Number(team2numbershoot.innerHTML)+1;
-    team2numbershoot.innerHTML=team2number;
+    let team2number = Number(team2shots.innerHTML)+1;
+    team2shots.innerHTML=team2number;
     if (Math.random() * 100 < 65 ) {
         let scoregoals2=Number(team2goals.innerHTML) + 1;
     team2goals.innerHTML = scoregoals2;
@@ -32,8 +39,8 @@ numberofResets.addEventListener("click", function () {
     console.log("reset");
     let numberofResets = Number(resetButton.innerHTML) + 1;
     resetButton.innerHTML = numberofResets;
-    team1numbershoot.innerHTML = 0
-    team2numbershoot.innerHTML = 0
+    team1shots.innerHTML = 0
+    team2shots.innerHTML = 0
     team1goals.innerHTML = 0
     team2goals.innerHTML = 0
     
